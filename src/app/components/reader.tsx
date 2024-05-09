a645f0954491c033cc63e772872340e1f0db91c8
Zulqarnain-cc34 <alphatech345@protonmail.com>
2024-05-09 07:50:25 +0500

    added the updated packages

--- src/app/components/reader.tsx
+++ src/app/components/reader.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { ReactReader } from 'react-reader'
 
-const EpubViewer = ({ url }) => {
+const EpubViewer = ({ url }:{url: any}) => {
 const [location, setLocation] = useState<string | number>(0)
   return (
     <div style={{ height: '100vh' }}>
